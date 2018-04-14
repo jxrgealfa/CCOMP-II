5f264d2a5f82bb5a652933a539dde4c4120c7a41
jxrgealfa <37647622+jxrgealfa@users.noreply.github.com>
2018-04-13 21:27:57 -0500

    avance

---
+++ Codificacion/main.cpp
@@ -0,0 +1,121 @@
+#include <iostream>
+#include <fstream>
+
+#include <string.h>
+using namespace std;
+
+string mensaje;
+int clave,tamano;
+int pes;
+
+int modulo(int a,int b){
+	int r=a-(a/b)*b;
+	if(r<0){
+		r=r+b;
+	}
+	return r;
+}
+
+int mod_inverso(int a,int b){
+	int p0=0,p1=1,pn=0,q,cont=0,total;
+	int r=a;
+	if((modulo(b,a))==0){return 0;}
+
+	else{
+        while(r>1){
+            cont=cont+1;
+            total=b;
+            b=r;
+            q=(total/b);
+            r=modulo(total,b);
+            pn=modulo((p0-p1*q),256);
+
+            p0=p1;
+            p1=pn;
+        }
+	}
+	return pn;
+}
+
+int escribiendo(int code[]){
+    ofstream escritura;
+    string elemento;
+    escritura.open("cipher.txt",ios::out);
+    for(int i=0;i<tamano;i++){
+            elemento=code[i];
+        escritura<<elemento;
+    }
+    if(escritura.is_open()){
+        cout<<"Abierto OK"<<endl;
+    }else{
+        cout<<"No se pudo abrir el archivo"<<endl;
+    }
+    escritura.close();
+}
+
+void descifrado(){
+    int publica;
+    cout<<"Ingrese la clave pública"<<endl;
+    cin>>publica;
+	ifstream lectura;
+	lectura.open("cipher.txt",ios::out|ios::in);
+	if(lectura.is_open()){
+        char i;
+		cout<<"Abierto OK lectura"<<endl;
+		cout<<"El mensaje es: "<<endl;
+
+        ofstream escritura;
+        escritura.open("plain2.txt",ios::out);
+        lectura>>i;
+		while(!lectura.eof()){
+            char k=char(modulo(int(i)*publica,256));
+            escritura<<k;
+            lectura>>i;
+		}
+		escritura.close();
+	}else{
+		cout<<"El archivo no se pudo leer"<<endl;
+	}
+}
+
+int cifrado(){
+    cout << "Ingrese una clave privada" << endl;
+    cin>>clave;
+    if(mod_inverso(clave,256)==0){cout<<"Esa clave no es permitida, elija otra"<<endl; return 0;}
+    ifstream lectura;
+	lectura.open("plain1.txt",ios::out|ios::in);
+	if(lectura.is_open()){
+	    char i;
+        i=lectura.get();
+        mensaje=i;;
+        int temp=0;
+        cout<<mensaje<<endl;
+        while(!lectura.eof()){
+            if(temp==1){
+                mensaje=mensaje+i;
+                i=lectura.get();
+                cout<<i<<endl;
+            }else{
+                temp=1;
+                i=lectura.get();
+                cout<<i<<endl;}
+		}
+		cout<<"Encuentre el mensaje descifrado en plain2.txt "<<endl;
+		cout<<mensaje<<endl;
+	}else{
+		cout<<"El archivo no se pudo leer"<<endl;
+	}
+    tamano=mensaje.length();
+    int code[tamano];
+    for(int i=0;i<tamano;i++){
+        code[i]=modulo((int(mensaje[i])*clave),256);
+        cout<<char(code[i]);
+    }
+    cout<<endl;
+    escribiendo(code);
+}
+
+int main(){
+    cifrado();
+    descifrado();
+}
--- Ejercicio4/main.cpp
+++ Ejercicio4/main.cpp
@@ -2,28 +2,53 @@
 
 using namespace std;
 
+void inversa(int a, int b, int c){
+    for(int b = 0; b<c; b++){
+        int x = (a*b)%c;
+        if(x<0){
+            x = x+c;
+        }
+        if(x==1){
+            cout<<" ->El resultado es: "<<b<<endl;
+            break;
+        }
+        else if(b==c-1 && x!=1){cout<<" ->No tiene inverso modular.\n";}
+    }
+}
+
 int Operacion(int op,int a,int b,int c){
     switch(op){
         case 1: return (a+b)%c;
         case 2: return (a-b)%c;
         case 3: return (a*b)%c;
+        case 4: inversa(a,b,c);
     }
 }
 
 int main()
 {
     int a,b,c,op,res;
-    cout<<"Ingrese la operacion:\n ->1 es suma.\n ->2 es resta.\n ->3 es multiplicacion."<<endl;
+    cout<<"Ingrese la operacion:\n ->1 es suma.\n ->2 es resta.\n ->3 es multiplicacion.\n ->4 es inversa."<<endl;
     cin>>op;
-    cout<<"Ingrese el primer numero:"<<endl;
-    cin>>a;
-    cout<<"Ingrese el segundo numero:"<<endl;
-    cin>>b;
-    cout<<"Ingrese el modulo numero:"<<endl;
-    cin>>c;
-    if(Operacion(op,a,b,c)<0){
-        res=Operacion(op,a,b,c)+c;
+    if(op==4){
+        cout<<"Ingrese el primer numero:"<<endl;
+        cin>>a;
+        cout<<"Ingrese el modulo numero:"<<endl;
+        cin>>c;
+        Operacion(op,a,b,c);
+    }else{
+        cout<<"Ingrese el primer numero:"<<endl;
+        cin>>a;
+        cout<<"Ingrese el segundo numero:"<<endl;
+        cin>>b;
+        cout<<"Ingrese el modulo numero:"<<endl;
+        cin>>c;
+        if(Operacion(op,a,b,c)<0){
+            res=Operacion(op,a,b,c)+c;
+        }else{
+            res=Operacion(op,a,b,c);
+        }
+        cout<<" ->El resultado es: "<<res<<"."<<endl;
     }
-    res=Operacion(op,a,b,c)+c;
-    cout<<" ->El resultado es: "<<res<<"."<<endl;
+
 }
